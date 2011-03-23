ac0025df1882200542e435d3c1b464ac9d079df2
Arthur Fonseca <arthurgfonseca@gmail.com>
2011-03-23 17:59:47 -0300

    CRUDs Arthur

--- ERP/ERP/CadastrarSatisfacaoCliente.xaml.cs
+++ ERP/ERP/CadastrarSatisfacaoCliente.xaml.cs
@@ -10,6 +10,7 @@ using System.Windows.Media;
 using System.Windows.Media.Animation;
 using System.Windows.Shapes;
 using System.Windows.Navigation;
+using ErpQualidadeModel;
 
 namespace ERP
 {
@@ -23,6 +24,41 @@ namespace ERP
         // Executes when the user navigates to this page.
         protected override void OnNavigatedTo(NavigationEventArgs e)
         {
+        }
+
+        private void satisfacaoClienteSetDomainDataSource_LoadedData(object sender, LoadedDataEventArgs e)
+        {
+
+            if (e.HasError)
+            {
+                System.Windows.MessageBox.Show(e.Error.ToString(), "Load Error", System.Windows.MessageBoxButton.OK);
+                e.MarkErrorAsHandled();
+            }
+        }
+        private void btnInserir_Click(object sender, RoutedEventArgs e)
+        {
+            ERP.Web.QualidadeContext QualidadeClienteContext = new Web.QualidadeContext();
+
+            try
+            {
+                SatisfacaoClienteSet novoCliente = new SatisfacaoClienteSet();
+
+                novoCliente.comentarios = comentariosTextBox.Text;
+                novoCliente.data_avaliacao = Convert.ToDateTime(data_avaliacaoDatePicker.Text);
+                novoCliente.nome = nomeTextBox.Text;
+                novoCliente.nota_final = Convert.ToDecimal(nota_finalTextBox.Text);
+                novoCliente.qualidade_atendimento = Convert.ToDecimal(qualidade_atendimentoTextBox.Text);
+                novoCliente.tempo_espera = Convert.ToDecimal(tempo_esperaTextBox.Text);
+
+                QualidadeClienteContext.SatisfacaoClienteSets.Add(novoCliente);
+                QualidadeClienteContext.SubmitChanges();
+
+            }
+            catch (Exception ex)
+            {
+            }
+
+
         }
 
     }
--- ERP/ERP/CadastrarSatisfacaoFuncionario.xaml.cs
+++ ERP/ERP/CadastrarSatisfacaoFuncionario.xaml.cs
@@ -10,6 +10,7 @@ using System.Windows.Media;
 using System.Windows.Media.Animation;
 using System.Windows.Shapes;
 using System.Windows.Navigation;
+using ErpQualidadeModel;
 
 namespace ERP
 {
@@ -24,6 +25,45 @@ namespace ERP
         protected override void OnNavigatedTo(NavigationEventArgs e)
         {
         }
+
+        private void satisfacaoFuncionarioSetDomainDataSource_LoadedData(object sender, LoadedDataEventArgs e)
+        {
+
+            if (e.HasError)
+            {
+                System.Windows.MessageBox.Show(e.Error.ToString(), "Load Error", System.Windows.MessageBoxButton.OK);
+                e.MarkErrorAsHandled();
+            }
+        }
+        private void btnInserir_Click(object sender, RoutedEventArgs e)
+        {
+            ERP.Web.QualidadeContext QualidadeFuncionarioContext = new Web.QualidadeContext();
+
+            try
+            {
+                SatisfacaoFuncionarioSet novoFuncionario = new SatisfacaoFuncionarioSet();
+
+                novoFuncionario.comentarios = comentariosTextBox.Text;
+                novoFuncionario.data_avaliacao = Convert.ToDateTime(data_avaliacaoDatePicker.Text);
+                novoFuncionario.codigo_funcionario = codigo_funcionarioTextBox.Text;
+                novoFuncionario.nota_final = Convert.ToDecimal(nota_finalTextBox.Text);
+                novoFuncionario.nome_funcionario = nome_funcionarioTextBox.Text;
+                novoFuncionario.nota_ambiente_trabalho = Convert.ToDecimal(nota_ambiente_trabalhoTextBox.Text);
+                novoFuncionario.nota_colegas_trabalho = Convert.ToDecimal(nota_colegas_trabalhoTextBox.Text);
+                novoFuncionario.nota_final = Convert.ToDecimal(nota_finalTextBox.Text);
+                novoFuncionario.nota_satisfacao_pessoal = Convert.ToDecimal(nota_satisfacao_pessoalTextBox.Text);
+
+                QualidadeFuncionarioContext.SatisfacaoFuncionarioSets.Add(novoFuncionario);
+                QualidadeFuncionarioContext.SubmitChanges();
+
+            }
+            catch (Exception ex)
+            {
+            }
+
+
+        }
+
 
     }
 }
